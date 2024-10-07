0cbedc63c0e03968eb4acaa2a0631e56575b3448
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-10-07 17:00:34 +0600

    addded template for default search form and search form and search page and working on functionality

--- inc/classes/assets.php
+++ inc/classes/assets.php
@@ -17,6 +17,7 @@ class Assets{
          * Actions.
          */
         add_action("wp_enqueue_scripts",[$this,"register_styles"]);
+        add_action("wp_enqueue_scripts",[$this,"register_scripts"]);
     }
 
     public function register_styles(){
@@ -24,9 +25,29 @@ class Assets{
      * Register Main Style
      */
     wp_register_style('simplecharm-style', get_stylesheet_uri(), array(), filemtime(SIMPLECHARM_DIR_PATH.'/style.css'), 'all');
+    wp_register_style('simplecharm-choices-style', SIMPLECHARM_DIR_URI . '/assets/css/choices/choices.min.css', array(), 1.00, 'all');
     /**
-     * Enqueue Main Style
+     * Enqueue Styles
      */
     wp_enqueue_style('simplecharm-style');
+    
+        if(is_search()){
+            wp_enqueue_style('simplecharm-choices-style');
+        }
+    }
+
+    public function register_scripts(){
+    /**
+     * Register Select2 Script
+     */
+    wp_register_script('simplecharm-choices-js', SIMPLECHARM_DIR_URI . '/assets/js/choices/choices.min.js', array(), 1.00, true);
+    wp_register_script('simplecharm-choices-init-js', SIMPLECHARM_DIR_URI . '/assets/js/choices-init.js', ['jquery', 'simplecharm-choices-js'], 1.00, true);
+    /**
+     * Enqueue Scripts
+     */
+        if(is_search()){
+            wp_enqueue_script('simplecharm-choices-js');
+            wp_enqueue_script('simplecharm-choices-init-js');
+        }
     }
 }
--- template-parts/search/search.php
+++ template-parts/search/search.php
@@ -1,1 +1,24 @@
-<h2>Great</h2>
+<?php
+/**
+ * Search Form With Different Filters
+ * @package SimpleCharm
+ * @since 1.4
+ */
+?>
+<form role="search" method="post" class="search-form">
+	<label class="search-field-container">
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search …" name="s">
+	</label>
+	<label class="select2-core-container">
+		<span class="screen-reader-text categories">Categories</span>
+		<select class="search-categories" name="categories[]" id="search-by-categories" multiple>
+		  <option value="uncategorized">Uncategorized</option>
+		  <option value="html">HTML</option>
+		</select>
+		<div id="selected-count"></div>
+	</label>
+	<label class="submit-button-container">
+		<input type="submit" class="search-submit" value="Search">
+	</label>
+</form>
