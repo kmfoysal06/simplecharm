628c950f7f9e386c97394ad4bce83aae5d84a696
kazi mohammad Foysal <95936171+kmfoysal06@users.noreply.github.com>
2024-12-18 15:07:58 +0600

    hotfix: Fixed A Security Issue Where The Get Data Displayed in Page Without Escaping

--- template-parts/search/search.php
+++ template-parts/search/search.php
@@ -11,7 +11,7 @@ $search = SIMPLECHARM_THEME\Inc\Classes\Search::get_instance();
 <form role="search" method="post" class="search-form" id="simplecharm-advanced-search-form">
 	<label class="search-field-container">
 		<span class="screen-reader-text">Search for:</span>
-		<input type="search" class="search-field" placeholder="Search …" name="s" value="<?php echo isset($_GET['s']) ? $_GET['s'] : '' ?>">
+		<input type="search" class="search-field" placeholder="Search …" name="s" value="<?php echo isset($_GET['s']) ? esc_attr($_GET['s']) : '' ?>">
 	</label>
 	<label class="simplecharm-multiselect-core-container">
 		<span class="screen-reader-text categories">Categories</span>
